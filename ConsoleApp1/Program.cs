b25a5b083c68056ac46fd7444e7984371faf7561
joel <joeldowono1@gmail.com>
2023-01-31 01:38:40 +0100

    Rajouter des ingredients

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,12 +11,14 @@ namespace MyApp
             string nom;
             float prix;
             bool vegetarienne;
+            List<string> ingredients;
 
-            public Pizza(string nom, float prix, bool vegetarienne)
+            public Pizza(string nom, float prix, bool vegetarienne, List<string> ingredients)
             {
                 this.nom = nom;
                 this.prix = prix;
                 this.vegetarienne = vegetarienne;
+                this.ingredients = ingredients;
             }
 
             public void Afficher()
@@ -32,11 +34,13 @@ namespace MyApp
                 string nomAfficher = FormatPremiereLettreMajuscules(nom);
 
                 Console.WriteLine(nomAfficher + badgeVegetarienne + " - " + prix + "€");
+                Console.WriteLine(string.Join(", ", ingredients));
+                Console.WriteLine();
             }
 
             private static string FormatPremiereLettreMajuscules(string s)
             {
-                if ((s == null) || (s.Length == 0))
+                if (string.IsNullOrEmpty(s))
                     return s;
                 string minuscules = s.ToLower();
                 string majuscules = s.ToUpper();
@@ -52,18 +56,19 @@ namespace MyApp
             Console.OutputEncoding = Encoding.UTF8;
 
             var pizzas = new List<Pizza>() {
-                new Pizza("4 fromages", 11.2f, true),
-                new Pizza("indienne", 10.5f, false),
-                new Pizza("mexicaine", 13f, false),
-                new Pizza("margarita", 8f, true),
-                new Pizza("calzone", 12f, false),
-                new Pizza("complète", 9.5f, false),
+                new Pizza("4 fromages", 11.2f, true, new List<string>{"cantal", "mozzarella", "fromage de chèvre", "gruyère"}),
+                new Pizza("indienne", 10.5f, false, new List<string>{"curry", "mozzarella", "poulet", "poivron", "oignon", "coriandre"}),
+                new Pizza("mexicaine", 13f, false, new List<string>{"boef", "mozzarella", "maïs", "tomates", "oignon", "coriandre"}),
+                new Pizza("margarita", 8f, true, new List<string>{"sauce tomate", "mozzarella", "basilic"}),
+                new Pizza("calzone", 12f, false, new List<string>{"tomate", "jambon", "persil", "fromage"}),
+                new Pizza("complète", 9.5f, false, new List<string>{"jambon", "oeuf", "fromage"}),
             };
 
             foreach(var pizza in pizzas)
             {
                 pizza.Afficher();
             }
+            Console.Clear();
         }
     }
 }
